c9c0d55d86880305eab075b12b505d2a1cd6bda4
ShakilBTL44 <shakil.pedhari@bosctechlabs.com>
2024-01-18 14:05:25 +0530

    Math of number

---
+++ MathOfNumbers.py
@@ -0,0 +1,17 @@
+firstNumber = 20
+secondNumber = 3
+# sum
+sum = firstNumber + secondNumber
+print(sum)
+
+# subtract
+print(firstNumber-secondNumber)
+
+# division
+print(firstNumber/secondNumber)
+
+# multiplication
+print(firstNumber*secondNumber)
+
+# mode
+print(firstNumber%secondNumber)
