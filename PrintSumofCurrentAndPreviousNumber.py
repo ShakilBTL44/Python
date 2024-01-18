61f063816c27d3079b6390d25edabc6931838f55
ShakilBTL44 <shakil.pedhari@bosctechlabs.com>
2024-01-18 14:06:42 +0530

    PrintSum of current And Previous Number

---
+++ PrintSumofCurrentAndPreviousNumber.py
@@ -0,0 +1,6 @@
+def printPreviousAndCurrent (rang):
+    for num in range(1,rang):
+        print(f"previous is ${num-1} and current is ${num} and sum is ${num-1+num}")
+
+
+printPreviousAndCurrent(10)
